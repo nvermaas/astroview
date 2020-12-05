502c6c7edf5f619abe5c6fb04c73512854c11757
Nico Vermaas <vermaas@astron.nl>
2020-12-05 07:33:32 +0100

    making collection serializer leaner

--- src/components/Main.js
+++ src/components/Main.js
@@ -199,7 +199,7 @@ function Main () {
                     <Route path="/details/:id" children={<ObservationDetailsForward />} />
                 </Switch>
             </div>
-            <footer><small> (C) 2020 - Nico Vermaas - version 1.11.3 - 4 dec 2020 - 21:00</small></footer>
+            <footer><small> (C) 2020 - Nico Vermaas - version 1.11.3 - 5 dec 2020 - 07:00</small></footer>
         </Router>
     );
 }
