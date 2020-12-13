4b2fe60fcd029729843e7e0c91cebfb572bc377c
Nico Vermaas <vermaas@astron.nl>
2020-12-13 15:53:38 +0100

    add hips functionality

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -31,6 +31,7 @@ export function NavigationBar() {
     const [ my_state , my_dispatch] = useGlobalReducer()
     let ASTROBASE_URL_ADMIN = ASTROBASE_URL + 'admin'
     let ASTROBASE_URL_JOBS = ASTROBASE_URL + 'admin/backend_app/job'
+    let ASTROBASE_URL_HIPS = 'https://uilennest.net/astrobase/hips/'
 
     return (
         <Navbar bg="dark" variant="dark">
@@ -43,11 +44,13 @@ export function NavigationBar() {
                 <Nav.Link as={NavLink} to="/observations">Observations</Nav.Link>
                 <Nav.Link as={NavLink} to="/collections">Collections</Nav.Link>
                 <Nav.Link as={NavLink} to="/projects">Parents</Nav.Link>
+                &nbsp;
 
+                <Nav.Link target="_blank" href={ASTROBASE_URL_JOBS}>Jobs: {my_state.nr_of_jobs}</Nav.Link>
+                <Nav.Link target="_blank" href={ASTROBASE_URL_HIPS}>HIPS</Nav.Link>
+                &nbsp;
                 <Nav.Link target="_blank" href={ASTROBASE_URL}>AstroBase</Nav.Link>
                 <Nav.Link target="_blank" href={ASTROBASE_URL_ADMIN}>Admin</Nav.Link>
-                <Nav.Link target="_blank" href={ASTROBASE_URL_JOBS}>Jobs: {my_state.nr_of_jobs}</Nav.Link>
-
             </Nav>
             <DoCommandButton title="Generate HIPS" command="hips" />&nbsp;
 
