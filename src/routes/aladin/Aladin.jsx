ab75a7f0afe2031e97d572b028625d657152fd14
Nico Vermaas <vermaas@astron.nl>
2021-04-24 15:10:12 +0200

    working on aladin highlighting

--- src/routes/aladin/Aladin.jsx
+++ src/routes/aladin/Aladin.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
+
 const Aladin = (props) => {
+    const [ highlightedObservation , setHighlightedObservation] = React.useState([]);
 
     React.useEffect(() => {
         let aladin = window.A.aladin('#aladin-lite-div', { survey: 'P/DSS2/color', fov:60 })
@@ -27,11 +29,13 @@ const Aladin = (props) => {
 
         let my_catalog = window.A.catalog({
             name: 'MyObservations',
+            shape : 'circle',
+            color : 'yellow',
             sourceSize: 20,
             //labelColumn: 'name',
             //displayLabel: true,
             onClick: 'showTable'});
-        aladin.addCatalog(my_catalog);
+
 
         props.data.forEach(function(observation){
 
@@ -57,6 +61,7 @@ const Aladin = (props) => {
             addToCatalog(my_catalog, observation)
 
         })
+        aladin.addCatalog(my_catalog);
 
         // in case of a single observation
         if (props.observation!==undefined) {
@@ -65,30 +70,96 @@ const Aladin = (props) => {
             addBoxesToOverlay(my_selected_overlay, props.observation,"yellow")
        }
 
+       // add a listener to aladin
+        // define function triggered when  a source is hovered
+        aladin.on('objectHovered', function(object) {
+
+            var msg;
+            if (object) {
+                msg = object.data.my_field_name;
+
+                // unhighlight the previous highlighted observation
+                let previous = highlightedObservation
+                if (previous) {
+
+                    // get quality
+                    // draw box in expected color
+
+                    try {
+                        addBoxesToOverlay(my_selected_overlay, previous, "red")
+                    } catch (e) {
+                    }
+                }
+
+                // highlight the observation under the mouse
+                let my_observation = object.data.my_observation
+                addBoxesToOverlay(my_selected_overlay, my_observation,"yellow")
+
+                // save the highlighted observation to the local state
+                setHighlightedObservation(my_observation)
+            }
+        });
+
     }, [])
 
 
-    const addToCatalog = (my_catalog, observation) => {
-        let url = "/astroview/details/"+observation.taskID
-
-        my_catalog.addSources([window.A.marker(
-            observation.field_ra,
-            observation.field_dec,
-            {popupTitle: '<a href="'+url+'">'+observation.taskID+' - '+observation.name+'</a>',
-                popupDesc: observation.target,
-
-            }
-        )]);
-    }
-
-    const addBoxesToOverlay = (my_overlay, observation, color) => {
+    // get the bounding box in world coordinates from an observation
+    const getBox = (observation) => {
         let coords = observation.box.split(',')
         let point1 = [coords[0],coords[1]]
         let point2 = [coords[2],coords[3]]
         let point3 = [coords[4],coords[5]]
         let point4 = [coords[6],coords[7]]
         let box = [point1,point2,point3,point4,point1]
+        return box
+    }
 
+
+    const addToCatalog = (my_catalog, observation) => {
+        let url = "/astroview/details/"+observation.taskID
+
+        let box = getBox(observation)
+
+        let source = window.A.source(
+            observation.field_ra,
+            observation.field_dec,
+            {
+                my_field_name: observation.field_name,
+                my_name: observation.name,
+                popupTitle: '<a href="'+url+'">'+observation.taskID+' - '+observation.name+'</a>',
+                popupDesc: observation.field_name,
+            }
+        )
+
+        let marker = [window.A.marker(
+            observation.field_ra,
+            observation.field_dec,
+
+            {
+                my_field_name: observation.field_name,
+                my_name: observation.name,
+                popupTitle: '<a href="'+url+'">'+observation.taskID+'</a>',
+                popupDesc: '<hr>'+observation.name +'<br>'+ observation.field_name,
+            },
+
+            {
+                my_field_name: observation.field_name,
+                taskID : observation.taskID,
+                my_observation : observation,
+            },
+        )]
+        //my_catalog.addSources(source);
+        my_catalog.addSources(marker);
+    }
+
+
+    const addBoxesToOverlay = (my_overlay, observation, color) => {
+        let box = getBox(observation)
+        my_overlay.add(window.A.polyline(box, {color: color, lineWidth: 1}));
+    }
+
+
+    const addBoxToOverlay = (my_overlay, box, color) => {
         my_overlay.add(window.A.polyline(box, {color: color, lineWidth: 1}));
     }
 
@@ -108,7 +179,10 @@ const Aladin = (props) => {
     }
 
     return (
-        <div id='aladin-lite-div' className="aladin"  />
+        <div>
+            <h2>{highlightedObservation.name}</h2>
+            <div id='aladin-lite-div' className="aladin"  />
+        </div>
     )
 }
 
