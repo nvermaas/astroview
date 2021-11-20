c5f5d851ca5be99638a124ecc424c532bb5a5be1
Nico Vermaas <vermaas@astron.nl>
2021-11-20 17:44:37 +0100

    add cutouts thumbnails

--- src/routes/cutouts/ImageThumbnail.js
+++ src/routes/cutouts/ImageThumbnail.js
@@ -3,25 +3,29 @@ import {Card, Button } from 'react-bootstrap'
 import { Link } from "react-router-dom"
 
 import { useGlobalReducer } from '../../contexts/GlobalContext';
-import { SET_ACTIVE_OBSERVATION } from '../../reducers/GlobalStateReducer'
+
+import { getDetailsIcon } from '../../utils/styling'
+
+
+import { SET_ACTIVE_OBSERVATION, SET_CURRENT_TASK_ID, SET_CURRENT_PROJECT } from '../../reducers/GlobalStateReducer'
 
 // display a single cutout directory on a card
 export default function ImageThumbnail(props) {
     const [ my_state , my_dispatch] = useGlobalReducer()
 
-    const handleObservationClick = (taskID) => {
-
-        //alert(cutout_directory.number_of_images)
+    const handleDetailsClick = (taskID) => {
         // dispatch current observation to the global store
-        //my_dispatch({type: SET_ACTIVE_OBSERVATION, current_cutout: cutout_directory})
-
+        my_dispatch({type: SET_CURRENT_TASK_ID, current_task_id: taskID})
+        my_dispatch({type: SET_CURRENT_PROJECT, current_project: taskID})
     }
 
     // generate the details link to forward to
-    const getLink = (taskID) => {
+    const getDetailsLink = (taskID) => {
         let details_link = "/details/"+taskID
         return details_link
     }
+
+    let icon = getDetailsIcon(handleDetailsClick,props.cutout.observation_taskID)
     return (
 
         <Card className="card-img-cutout">
@@ -29,8 +33,12 @@ export default function ImageThumbnail(props) {
                       width="300" height="300" />
 
             <Card.ImgOverlay>
-                <Link to={() => getLink(props.cutout.observation_taskID)}>
-                    <Button variant="outline-warning" onClick={() => handleObservationClick(props.cutout.observation_taskID)}>{props.cutout.observation_taskID}</Button>&nbsp;
+                <Link to={() => getDetailsLink(props.cutout.observation_taskID)}>
+                    <div>{icon}</div>
+                </Link>
+
+                <Link to={() => getDetailsLink(props.cutout.observation_taskID)}>
+                    <Button variant="outline-warning" onClick={() => handleDetailsClick(props.cutout.observation_taskID)}>{props.cutout.observation_taskID}</Button>&nbsp;
                 </Link>
             </Card.ImgOverlay>
 
