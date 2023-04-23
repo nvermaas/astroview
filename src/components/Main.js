ffa6695456779abc40bc6cc4e3f87fa811a22eb4
Vermaas <vermaas@astron.nl>
2023-04-23 09:32:47 +0200

    update dependencies

--- src/components/Main.js
+++ src/components/Main.js
@@ -203,7 +203,7 @@ function Main () {
                 </Switch>
             </div>
 
-            <footer><small> (C) 2023 - Nico Vermaas - version 23 april 2023 - 9:00</small></footer>
+            <footer><small> (C) 2023 - Nico Vermaas - version 23 april 2023 - 9:30</small></footer>
             </Container>
         </Router>
     );
