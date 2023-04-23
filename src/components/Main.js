3478124ff97e5312af982b02075f16e4a55a530b
Vermaas <vermaas@astron.nl>
2023-04-23 10:21:29 +0200

    wow, did I get node 18 working?

--- src/components/Main.js
+++ src/components/Main.js
@@ -203,7 +203,7 @@ function Main () {
                 </Switch>
             </div>
 
-            <footer><small> (C) 2023 - Nico Vermaas - version 23 april 2023 - 10:00</small></footer>
+            <footer><small> (C) 2023 - Nico Vermaas - version 23 april 2023 - 10:20</small></footer>
             </Container>
         </Router>
     );
