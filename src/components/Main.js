dab4a9fbf1fa28a39dd4de8725f325b22be4c3ca
Vermaas <vermaas@astron.nl>
2026-07-11 20:14:26 +0200

    test deploy to woodland

--- src/components/Main.js
+++ src/components/Main.js
@@ -203,7 +203,7 @@ function Main () {
                 </Switch>
             </div>
 
-            <footer><small> (C) 2025 - Nico Vermaas - version 11 Jul 2026</small></footer>
+            <footer><small> (C) 2025 - Nico Vermaas - version 11 Jul 2026 - 20:00</small></footer>
             </Container>
         </Router>
     );
