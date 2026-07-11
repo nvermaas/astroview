409b047532a4c83b3b2a34a38b75edfd74eed92e
Vermaas <vermaas@astron.nl>
2026-07-11 20:56:38 +0200

    make url's detect on which host they run

--- src/utils/skyserver.js
+++ src/utils/skyserver.js
@@ -6,13 +6,15 @@
 // http://skyserver.sdss.org/dr12/en/help/docs/api.aspx#RestWebServices
 
 export const EXOPLANETS_URL = "https://uilennest.net/exoplanets/"
+export const EXOPLANETS_URL = `${window.location.origin}/exoplanets/`
 export const ALADIN_STARCHARTS_URL = "https://uilennest.net/aladin-starcharts/"
+export const ALADIN_STARCHARTS_URL = `${window.location.origin}/aladin-starcharts/`
 
 export const ASTROBASE_URL =
     process.env.NODE_ENV === "development"
         ? "http://localhost:8000/my_astrobase/"
         //? "https://uilennest.net/my_astrobase/"
-        : "https://uilennest.net/my_astrobase/";
+        : `${window.location.origin}/my_astrobase/`;
 
 
 
